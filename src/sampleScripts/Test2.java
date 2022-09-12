2ffde0b97942e33b3afd2cc96734216ee28a67c1
ramprasadsuthi <ramprasadsuthi@gmail.com>
2022-09-12 12:02:23 +0530

    New Test2 File

---
+++ src/sampleScripts/Test2.java
@@ -0,0 +1,12 @@
+package sampleScripts;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(" This is a Sample Test Program for GITHUB");
+
+	}
+
+}
