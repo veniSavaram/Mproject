975ffe359aa52ea930d390b854f9010128b56d57
ramprasadsuthi <59646329+ramprasadsuthi@users.noreply.github.com>
2022-12-06 12:19:34 +0530

    Update LoginTC111.java

--- src/testScripts/LoginTC111.java
+++ src/testScripts/LoginTC111.java
@@ -18,6 +18,7 @@ public class LoginTC111 {
 		base.launchERP();
 		base.LoginERP();
 		System.out.println("This line I am adding from Eclipse (Local work space..)");
+	  	System.out.println("This line I am adding from GITHUB Server..");
 		
 		System.out.println("This TC is related to Login Test Case..");
 	        System.out.println("This line is added from GITHUB Server..");
